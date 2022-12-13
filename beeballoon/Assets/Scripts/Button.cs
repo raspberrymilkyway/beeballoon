e952ad16dc77310cfda0d33de619bef173443b42
Kylee W <23221736+raspberrymilkyway@users.noreply.github.com>
2022-12-12 22:15:19 -0600

    ui-ish

---
+++ beeballoon/Assets/Scripts/Button.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void pleasePause(){
+        Debug.Log("pause button pressed");
+    }
+}
