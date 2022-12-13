a2234e1147eac52501fab4f74d5254d98fc4a347
Kylee W <23221736+raspberrymilkyway@users.noreply.github.com>
2022-12-12 23:57:13 -0600

    audio UI

--- beeballoon/Assets/Scripts/Button.cs
+++ beeballoon/Assets/Scripts/Button.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Button : MonoBehaviour
 {
     public void pleasePause(){
         Debug.Log("pause button pressed");
+    }
+
+    public void audioToggle(){
+        Image audioOn = GameObject.Find("audioOn").GetComponent<Image>();
+        //Debug.Log("found audioOn");
+        Image audioOff = GameObject.Find("audioOff").GetComponent<Image>();
+        //Debug.Log("found audioOff");
+
+        if (audioOff.enabled){
+            audioOn.enabled = true;
+            audioOff.enabled = false;
+        }
+        else{
+            audioOn.enabled = false;
+            audioOff.enabled = true;
+        }
     }
 }
