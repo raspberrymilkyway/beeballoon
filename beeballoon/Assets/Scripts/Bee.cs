de3092d40a675ef824035a4cc1060baf3634c9b0
Kylee W <23221736+raspberrymilkyway@users.noreply.github.com>
2022-12-12 22:36:24 -0600

    lives

--- beeballoon/Assets/Scripts/Bee.cs
+++ beeballoon/Assets/Scripts/Bee.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Bee : MonoBehaviour
 {
@@ -11,6 +12,11 @@ public class Bee : MonoBehaviour
     public float speed = 1;
 
     [Header("Already Set")]
+    public int totalLives = 3; //don't change this without changing ui
+    public Image beeTop;
+    public Image beeMid;
+    public Image beeBot;
+
     private Rigidbody rb;
     private Vector3[] moves;
     private KeyCode[] arrows = new KeyCode[] {KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.DownArrow};
@@ -18,6 +24,9 @@ public class Bee : MonoBehaviour
     private SpriteRenderer sprend;
 
     private int facing = 1;
+    private int livesLeft;
+    private float xStart;
+    private float yStart;
 
     void Start()
     {
@@ -25,7 +34,12 @@ public class Bee : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         sprend = GetComponent<SpriteRenderer>();
         moves = new Vector3[] {new Vector3(-speed, 0, 0), new Vector3(speed, 0, 0), new Vector3(0, speed, 0), new Vector3(0, -speed, 0)};
-        
+        beeTop.enabled = true;
+        beeMid.enabled = true;
+        beeBot.enabled = true;
+        livesLeft = totalLives;
+        xStart = transform.position.x;
+        yStart = transform.position.y;
     }
 
     
@@ -58,10 +72,26 @@ public class Bee : MonoBehaviour
     }
 
     void OnCollisionEnter(Collision coll) {
-        Debug.Log(coll.gameObject.tag);
+        //Debug.Log(coll.gameObject.tag);
         if(coll.gameObject.tag == "redboxofdoom" || coll.gameObject.tag == "bomb") {
             //Destroy(this.gameObject);
-            SceneManager.LoadScene("LevelOne");
+            Vector3 ori = new Vector3(xStart, yStart, 0);
+            livesLeft--;
+            if (livesLeft == 2){
+                beeTop.enabled = false;
+                transform.position = ori;
+            }
+            else if (livesLeft == 1){
+                beeMid.enabled = false;
+                transform.position = ori;
+            }
+            else if (livesLeft == 0){
+                beeBot.enabled = false;
+                transform.position = ori;
+            }
+            else{
+                SceneManager.LoadScene("LevelOne");
+            }
         }
     }
 }
